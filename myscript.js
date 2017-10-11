d600cfcbbd2c64abf9d492ffb87e4d54e1d0cc2f
신현규 <yougatup@sinhyeongyuui-MacBook.local>
2017-10-12 01:11:40 +0900

    prompting message refinement

--- myscript.js
+++ myscript.js
@@ -11,6 +11,8 @@ let category = ''
 let questions = function () {
   $('#myContainer').empty()
   let str = '<h3>' + promise.title + '</h3><br>'  
+  str += '<ul> <li> 생활권 주변 10분거리 공원과 생애주기별 힐링공원 조성 </li> <li> 서울둘레길 근교산 자락길 등 걷고 싶은 서울길 완성 </li> </ul> <br> <hr />'
+
   str += '<p>' + qs[qidx].content + '</p>'
   if (qs[qidx].type !== 'end') {
     str += qs[qidx].vueElements.reduce(function (prev, cur) {
@@ -19,9 +21,9 @@ let questions = function () {
     str += '<p>' + qs[qidx].extraContent + '</p>'
     if (qs[qidx].type === 'status') {
       str += '<button class="buttons progressButtons">역행중</button> <button class="buttons progressButtons">이행되지 않음</button> '         
-      str += '<button class="buttons progressButtons">이행 중</button> <button class="buttons progressButtons">이행 완료</button>'                  
+      str += '<button class="buttons progressButtons">이행 중</button> <button class="buttons progressButtons">이행 완료</button> <br><br> <button class="buttons dontknowProgress">잘 모르겠음</button>'                  
     } else {
-      str += '<button class="buttons yesButton">예</button> <button class="buttons noButton">아니오</button>'      
+      str += '<button class="buttons yesButton">예</button> <button class="buttons noButton">아니오</button> <button class="buttons dontknow">잘 모르겠음</button>'      
     }
   } else {
     str += '<input type="text"> <button class="submitButton">의견 보내기</button>'
@@ -41,7 +43,7 @@ let questions = function () {
   })
 }
 let addButtons = function () {
-  $('#articleBodyContents').append('<div id="myContainer" title="공약 더 알아보기"><h3>기사와 관련있는 ' + officialName + '의 ' + category  + ' 분야 공약입니다. 클릭하시면 자세한 내용을 알아보실 수 있습니다.</h3></div>')
+  $('#articleBodyContents').append('<div id="myContainer" title="PromiseBook"><h3>기사와 관련있는 ' + officialName + '의 공약입니다. 클릭하시면 자세한 내용을 알아보실 수 있습니다.</h3></div>')
   $('#myContainer').dialog({
     position: {
       my: 'right center',
@@ -52,7 +54,7 @@ let addButtons = function () {
   promise = promises[0]//promises[Math.floor(Math.random() * promises.length)]
   qs = [
     {
-      content: '이 공약이 있었다는 것을 알고 계셨나요?',
+      content: '지금 보고 계신 기사가 해당 공약 이행과 관련이 있어 보이시나요?',
       vueElements: [],
       extraContent: '',
       type: 'purpose'
@@ -70,16 +72,16 @@ let addButtons = function () {
     //   type: 'plan'
     // },
     {
+      content: '이 공약이 우리나라를 투명한 국가로 만드는 데 도움이 된다고 생각하시나요?',
+      vueElements: [],
+      extraContent: '',
+      type: 'plan'
+    },
+    {
       content: '기사를 읽고, 이 공약의 현재 상태를 평가해주세요.',
       vueElements: [],
       extraContent: '',
       type: 'status'
-    },
-    {
-      content: '이 공약은 좋은 공약이라고 생각하시나요?',
-      vueElements: [],
-      extraContent: '',
-      type: 'plan'
     },
     // {
     //   content: '다음은 이 공약과 연관된 자료입니다.',
@@ -101,7 +103,7 @@ let addButtons = function () {
     //   type: 'comments'
     // },
     {
-      content: '응답해주셔서 감사합니다. 공약에 대해 ' + officialName +  '에게 하실 말씀이 있으시다면 남겨주세요.',
+      content: officialName +  '에게 공약 이행 현황 공개를 요청하는 메시지를 보내주세요. 이 메시지는 ' + officialName + '에게 전달되며, PromiseBook 홈페이지에도 게시됩니다.',
       extraContent: '',//'다른 공약 보기 버튼을 누르시면 기사와 연관있는 다른 공약을 보실 수 있습니다.',
       type: 'end'
     }
@@ -128,7 +130,7 @@ var initializePromiseList = function () {
   // let data = new FormData()
   // data.append('article', article)
   let httpPost = new XMLHttpRequest()
-  let url = 'http://34.208.245.104:3000/promise/seoul/0'
+  let url = 'http://34.208.245.104:3000/promise/seoul/0
   httpPost.onreadystatechange = function (err) {
     if (httpPost.readyState == 4 && httpPost.status == 200) {
       let response = JSON.parse(httpPost.responseText)
